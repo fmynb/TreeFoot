1a2a7f22dd0115e766c4aad32d1c656578263d6a
张驰 <zhangchi.cone@bytedance.com>
2020-09-15 15:18:10 +0800

    init

---
+++ TreeFoot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TreeFoot
+//
+//  Created by zhangchi on 2020/9/15.
+//  Copyright © 2020 Hut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
