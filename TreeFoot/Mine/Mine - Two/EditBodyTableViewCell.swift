0d698d7cd253f5cab0106e5a3f254973b0c0b57d
fmynb <1327504928@qq.com>
2020-09-23 19:38:33 +0800

    编辑资料

--- TreeFoot/Mine/Mine - Two/EditBodyTableViewCell.swift
+++ TreeFoot/Mine/Mine - Two/EditBodyTableViewCell.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class EditBodyTableViewCell: UITableViewCell {
-
+     
+    
+    lazy var lefttext:UILabel = {
+      let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    
+    lazy var centertext:UILabel = {
+      let label = UILabel()
+        return label
+    }()
+    
+   
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -29,7 +43,21 @@ class EditBodyTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     func configUI() {
-        
+        self.addSubview(lefttext)
+        lefttext.snp.makeConstraints{ (make) in
+            make.width.equalTo(84.fit)
+            make.height.equalTo(22.fit)
+            make.left.equalToSuperview().offset(22.fit)
+            make.top.equalToSuperview().offset(15.fit)
+        }
+        self.addSubview(centertext)
+        centertext.snp.makeConstraints{ (make) in
+            make.width.equalTo(104.fit)
+            make.height.equalTo(22.fit)
+            make.left.equalToSuperview().offset(103.fit)
+            make.top.equalToSuperview().offset(15.fit)
     }
 
+}
+    
 }
